0d6299dce7c1ef08a4c214966a88a503be61ceab
irbujam <158004646+irbujam@users.noreply.github.com>
2024-10-17 19:47:23 -0300

    Update mount_disks.sh

--- mount_disks.sh
+++ mount_disks.sh
@@ -3,6 +3,8 @@
 ### Backup fstab
 ##cp /etc/fstab /etc/fstab.bak
 
+b_printed_comment_header=false
+
 # check if OS/Boot drive
 fCheckDiskType_OS_Boot() {
     local _disk=$1
@@ -14,9 +16,9 @@ fCheckDiskType_OS_Boot() {
         _ret_val=0  # Found OS/Boot drive
     else
         if [ "/dev/${_disk}" == "${_os_drive}" ]; then
-	    _ret_val=0  # Found device mounted on root fielsystem
-	else
-	    _ret_val=1  # Found Non OS/Boot drive
+            _ret_val=0  # Found device mounted on root fielsystem
+        else
+            _ret_val=1  # Found Non OS/Boot drive
         fi
     fi
     #
@@ -33,56 +35,69 @@ fAddTo_fstab() {
     echo "UUID=${uuid} ${mount_point} ${fs_type} defaults,noatime,nofail 0 0" >> /etc/fstab
 }
 
-clear
-# get drive info mounted on root filesystem
-os_drive=$(df / | tail -1 | awk '{print $1}')
-# Get list of unmounted disks
-unmounted_disks=$(lsblk -o NAME,MOUNTPOINT | grep '^[^ ]* *$' | awk '{print $1}')
+fMain() {
+        clear
+        # get drive info mounted on root filesystem
+        os_drive=$(df / | tail -1 | awk '{print $1}')
 
-echo '##----------------------------------------------------##' >> /etc/fstab
-echo "## >>>>>>>>  Begin : Subspace definitions   <<<<<<<<< ## " >> /etc/fstab
-echo '##----------------------------------------------------##' >> /etc/fstab
-# process unmounted disk(s)
-for disk in $unmounted_disks; do
-    # check OS/Boot drive
-    if fCheckDiskType_OS_Boot "${disk}" "${os_drive}"; then
-       echo "Skipped: /dev/${disk}...Reason: OS/Boot drive"
-       echo ""
-       continue
-    fi
+        # Get list of unmounted disks
+        unmounted_disks=$(lsblk -o NAME,MOUNTPOINT | grep '^[^ ]* *$' | awk '{print $1}')
 
-    # Get UUID
-    uuid=$(blkid -s UUID -o value /dev/"${disk}")
+        # process unmounted disk(s)
+        for disk in $unmounted_disks; do
+            # check OS/Boot drive
+            if fCheckDiskType_OS_Boot "${disk}" "${os_drive}"; then
+                echo "Skipped: /dev/${disk}...Reason: OS/Boot drive"
+                echo ""
+                continue
+            fi
 
-    if [ -n "$uuid" ]; then
-        # get drive size info
-	#disk_size=$(lsblk -b --output SIZE -n -d /dev/"${disk}")
-	disk_size=$(lsblk --output SIZE -n -d /dev/"${disk}" | xargs)
+            # Get UUID
+            uuid=$(blkid -s UUID -o value /dev/"${disk}")
 
-        # Create a mount point
-        mount_point="/mnt/subspace/${disk}"
-	mkdir -p "${mount_point}"
+            if [ -n "$uuid" ]; then
+                #
+                if [ $b_printed_comment_header == false ]; then
+                        b_printed_comment_header=true
+                        echo '##----------------------------------------------------##' >> /etc/fstab
+                        echo "## >>>>>>>>  Begin : Subspace definitions   <<<<<<<<< ## " >> /etc/fstab
+                        echo '##----------------------------------------------------##' >> /etc/fstab
+                fi
 
-        # Assume ext4 filesystem type, change if needed
-        #fs_type="ext4"
-        fs_type=$(blkid -s TYPE -o value /dev/"${disk}")
+                # get drive size info
+                #disk_size=$(lsblk -b --output SIZE -n -d /dev/"${disk}")
+                disk_size=$(lsblk --output SIZE -n -d /dev/"${disk}" | xargs)
 
-        # Add entry to fstab
-        fAddTo_fstab "${uuid}" "${mount_point}" "${fs_type}"
-	echo "Disk: /dev/${disk}, UUID: ${uuid}, Size: ${disk_size}"
-	echo "Action Taken: Added ${uuid} to fstab with mount point ${mount_point}"
-	echo ""
-    else
-        echo "No UUID found for /dev/$disk"
-    fi
-done
+                # Create a mount point
+                mount_point="/mnt/subspace/${disk}"
+                mkdir -p "${mount_point}"
 
-#execute disk(s) mount
-mount -av
-systemctl daemon-reload
+                # Get filesystem type
+                fs_type=$(blkid -s TYPE -o value /dev/"${disk}")
 
-echo '##----------------------------------------------------##' >> /etc/fstab
-echo "## >>>>>>>>   End - Subspace definitions    <<<<<<<<< ## " >> /etc/fstab
-echo '##----------------------------------------------------##' >> /etc/fstab
+                # Add entry to fstab
+                fAddTo_fstab "${uuid}" "${mount_point}" "${fs_type}"
+
+                # display to console
+                echo "Disk: /dev/${disk}, UUID: ${uuid}, Size: ${disk_size}"
+                echo "Action Taken: Added ${uuid} to fstab with mount point ${mount_point}"
+                echo ""
+            else
+                echo "No UUID found for /dev/$disk"
+            fi
+        done
+
+        if [ $b_printed_comment_header == true ]; then
+                echo '##----------------------------------------------------##' >> /etc/fstab
+                echo "## >>>>>>>>   End - Subspace definitions    <<<<<<<<< ## " >> /etc/fstab
+                echo '##----------------------------------------------------##' >> /etc/fstab
+
+                #execute disk(s) mount
+                mount -av
+                systemctl daemon-reload
+        fi
+}
 
 echo "Finished...please check /etc/fstab for the new entries."
+
+fMain
